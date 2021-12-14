817054abfaeea1602f00b1a0e498f3ccc77894aa
Zach Weaver <49159969+Kiyoshika@users.noreply.github.com>
2021-12-13 21:54:28 -0500

    revamped metric for entropy; added penalty term

--- Entropy Based Model Selection.R
+++ Entropy Based Model Selection.R
@@ -1,130 +1,98 @@
-# Generate arbitrary regression model
-y = c()
+# generate synthetic data
 x1 = c()
 x2 = c()
-for (i in 1:200)
-{
-  x1[i] = rnorm(1, mean = 2, sd = 2)
-  x2[i] = rnorm(1, mean = 4, sd = 3)
-  y[i] = 3.3 * x1[i] + 4.8 * x2[i] + rnorm(1, mean = 0, sd = 5)
-}
-
-
-
-# Plot Data
-full_data <- cbind(as.data.frame(x1), as.data.frame(x2), as.data.frame(y))
-plot(x1, y, pch=19, lwd=3)
-plot(x2, y, pch=19, lwd=3)
-
-
-
-# Generate test data
-y_t = c()
-x1_t = c()
-x2_t = c()
+x3 = c()
+x4 = c()
+y = c()
 for (i in 1:1000)
 {
-  x1_t[i] = rnorm(1, mean = 2, sd = 2)
-  x2_t[i] = rnorm(1, mean = 4, sd = 3)
-  y_t[i] = 3.3 * x1_t[i] + 4.8 * x2_t[i] + rnorm(1, mean = 0, sd = 5)
+  x1[i] = rnorm(1, mean = 2, sd = 1)
+  x2[i] = rnorm(1, mean = 5, sd = 3.5)
+  x3[i] = rnorm(1, mean = -1, sd = 2)
+  x4[i] = rnorm(1, mean = 3, sd = 2)
+  y[i] = 3*x1[i] + 1*x2[i] + 0*x3[i] + 0*x4[i] + 10 + rnorm(1, mean = 0, sd = 4)
 }
 
+# naive mean model (baseline)
+mu_y = mean(y)
 
+# scale the data for the penalizer term
+x1_scaled = (x1 - mean(x1))/sd(x1)
+x2_scaled = (x2 - mean(x2))/sd(x2)
+x3_scaled = (x3 - mean(x3))/sd(x3)
+x4_scaled = (x4 - mean(x4))/sd(x4)
 
-# Models
-full_model <- lm(y ~ x1 + x2, data = full_data)
-full_model$coefficients
+# fit models and evaluate entropy
 
-x1_model <- lm(y ~ x1, data = full_data)
-x1_model$coefficients
+# NOTE: the scaled models are only used for evaluating penalty term; not predictions.
 
-x2_model <- lm(y ~ x2, data = full_data)
-x2_model$coefficients
+# NOTE 2: the "penalizer" takes the reciprocal of mean of the absolute value of the (scaled) coefficient estimates (excluding the intercept).
+# This is because the larger the coefficients, the closer this metric will be to zero and vice versa, if the coefficients are
+# close to zero then this metric grows large (and is infinity if any of the coefficients are exactly zero, which could be one
+# potential complication).
 
+# If you have a large model with a mot of redundant coefficients (near-zero), then this will drag the metric towards zero
+# and thus increase penalty. If you have a large model with a lot of informative coefficients (away from zero) then this will
+# drag the metric away from zero and thus decrease the penalty.
+full_mod = lm(y ~ x1 + x2 + x3 + x4)
+full_mod_scaled = lm(y ~ x1_scaled + x2_scaled + x3_scaled + x4_scaled)
+full_mod_penalizer = (1 / mean(abs(full_mod_scaled$coefficients[-1]))) # ignore intercept
+full_mod_entropy = full_mod_penalizer + -1*mean(log2(dnorm(y, mean = predict.lm(full_mod), sd = sd(residuals(full_mod)))))
 
+x12_mod = lm(y ~ x1 + x2)
+x12_mod_scaled = lm(y ~ x1_scaled + x2_scaled)
+x12_mod_penalizer = (1 / mean(abs(x12_mod_scaled$coefficients[-1]))) # ignore intercept
+x12_mod_entropy = x12_mod_penalizer + -1*mean(log2(dnorm(y, mean = predict.lm(x12_mod), sd = sd(residuals(x12_mod)))))
 
-# Evaluate Models
-evaluate_full_model <- function(x1, x2)
+x123_mod = lm(y ~ x1 + x2 + x3)
+x123_mod_scaled = lm(y ~ x1_scaled + x2_scaled + x3_scaled)
+x123_mod_penalizer = (1 / mean(abs(x123_mod_scaled$coefficients[-1]))) # ignore intercept
+x123_mod_entropy = x123_mod_penalizer + -1*mean(log2(dnorm(y, mean = predict.lm(x123_mod), sd = sd(residuals(x123_mod)))))
+
+x13_mod = lm(y ~ x1 + x3)
+x13_mod_scaled = lm(y ~ x1_scaled + x3_scaled)
+x13_mod_penalizer = (1 / mean(abs(x13_mod_scaled$coefficients[-1]))) # ignore intercept
+x13_mod_entropy = x13_mod_penalizer + -1*mean(log2(dnorm(y, mean = predict.lm(x13_mod), sd = sd(residuals(x13_mod)))))
+
+x34_mod = lm(y ~ x3 + x4)
+x34_mod_scaled = lm(y ~ x3_scaled + x4_scaled)
+x34_mod_penalizer = (1 / mean(abs(x34_mod_scaled$coefficients[-1]))) # ignore intercept
+x34_mod_entropy = x34_mod_penalizer + -1*mean(log2(dnorm(y, mean = predict.lm(x34_mod), sd = sd(residuals(x34_mod)))))
+
+full_mod_entropy
+x12_mod_entropy
+x123_mod_entropy
+x13_mod_entropy
+x34_mod_entropy
+
+# as a sanity check, we can compare against AIC
+AIC(full_mod)
+AIC(x12_mod)
+AIC(x123_mod)
+AIC(x13_mod)
+AIC(x34_mod)
+
+# generate "unseen" data
+x1 = c()
+x2 = c()
+x3 = c()
+x4 = c()
+y = c()
+for (i in 1:1000)
 {
-  newdata = cbind(as.data.frame(x1), as.data.frame(x2))
-  predict.lm(full_model, newdata)
+  x1[i] = rnorm(1, mean = 2, sd = 1)
+  x2[i] = rnorm(1, mean = 5, sd = 3.5)
+  x3[i] = rnorm(1, mean = -1, sd = 2)
+  x4[i] = rnorm(1, mean = 3, sd = 2)
+  y[i] = 3*x1[i] + 1*x2[i] + 0*x3[i] + 0*x4[i] + 10 + rnorm(1, mean = 0, sd = 4)
 }
 
-evaluate_x1_model <- function(x1)
-{
-  newdata = as.data.frame(x1)
-  predict.lm(x1_model, newdata)
-}
+# evaluate root mean squared error
+test_data = as.data.frame(cbind(x1, x2, x3, x4))
 
-evaluate_x2_model <- function(x2)
-{
-  newdata = as.data.frame(x2)
-  predict.lm(x2_model, newdata)
-}
-
-
-
-# Compute Likelihoods
-fit1 <- c()
-for (i in 1:nrow(full_data))
-{
-  fit1[i] = dnorm(y[i], mean = evaluate_full_model(x1[i], x2[i]), sd = sd(residuals(full_model)))
-}
-
-fit2 <- c()
-for (i in 1:nrow(full_data))
-{
-  fit2[i] = dnorm(y[i], mean = evaluate_x1_model(x1[i]), sd = sd(residuals(x1_model)))
-}
-
-fit3 <- c()
-for (i in 1:nrow(full_data))
-{
-  fit3[i] = dnorm(y[i], mean = evaluate_x2_model(x2[i]), sd = sd(residuals(x2_model)))
-}
-
-fit4 <-c() # default model (mean of dependent variable)
-for (i in 1:nrow(full_data))
-{
-  fit4[i] = dnorm(y[i], mean = mean(y), sd = sd(y - mean(y)))
-}
-
-
-
-# Compute Entropy (the lower the better)
-# H(X) = -E[log f(x)], f(x) being the likelihood
-# lower entropy corresponds to more predictability within a probabilistic system
--mean(log(fit1))
--mean(log(fit2))
--mean(log(fit3))
--mean(log(fit4))
-
-
-
-# For comparison, we can display the AIC values
-# don't have a model for default case (mean) so I'm ignoring it here, but 
-# theoretically should be similar to x2_model
-AIC(full_model)
-AIC(x1_model)
-AIC(x2_model)
-
-
-
-library(dplyr)
-# Evaluate models against test data
-full_test_data <- cbind(as.data.frame(x1_t), as.data.frame(x2_t), as.data.frame(y_t))
-names(full_test_data) <- c("x1","x2","y")
-
-x1_test_data = full_test_data %>% select(x1)
-x2_test_data = full_test_data %>% select(x2)
-y_test_data = full_test_data %>% select(y)
-
-full_model_preds = predict.lm(full_model, newdata = full_test_data)
-x1_model_preds = predict.lm(x1_model, newdata = x1_test_data)
-x2_model_preds = predict.lm(x2_model, newdata = x2_test_data)
-
-# Mean absolute error
-mean(unlist(abs(full_model_preds - y_test_data)))
-mean(unlist(abs(x1_model_preds - y_test_data)))
-mean(unlist(abs(x2_model_preds - y_test_data)))
-mean(unlist(abs(mean(y) - y_test_data)))
+sqrt(mean((y - predict.lm(full_mod, newdata = test_data))^2))
+sqrt(mean((y - predict.lm(x12_mod, newdata = test_data))^2))
+sqrt(mean((y - predict.lm(x123_mod, newdata = test_data))^2))
+sqrt(mean((y - predict.lm(x13_mod, newdata = test_data))^2))
+sqrt(mean((y - predict.lm(x34_mod, newdata = test_data))^2))
+sqrt(mean((y - mu_y)^2))
