72a5b56fcff47cb23014770f1d1b935295539482
Zach Weaver <49159969+Kiyoshika@users.noreply.github.com>
2021-12-18 13:43:00 -0500

    revamped penalty term

--- Entropy Based Model Selection.R
+++ Entropy Based Model Selection.R
@@ -7,72 +7,99 @@ y = c()
 for (i in 1:1000)
 {
   x1[i] = rnorm(1, mean = 2, sd = 1)
-  x2[i] = rnorm(1, mean = 5, sd = 3.5)
-  x3[i] = rnorm(1, mean = -1, sd = 2)
-  x4[i] = rnorm(1, mean = 3, sd = 2)
-  y[i] = 3*x1[i] + 1*x2[i] + 0*x3[i] + 0*x4[i] + 10 + rnorm(1, mean = 0, sd = 4)
+  x2[i] = rnorm(1, mean = -1, sd = 2)
+  x3[i] = rnorm(1, mean = 3.5, sd = 2.2)
+  x4[i] = rnorm(1, mean = 1, sd = 4)
+  y[i] = 3*x1[i] + 2*x2[i] + 0*x3[i] + 0*x4[i] + 10 + rnorm(1, mean = 0, sd = 4)
 }
+plot(x1,y)
+plot(x2,y)
+plot(x3,y)
+plot(x4,y)
 
-# naive mean model (baseline)
-mu_y = mean(y)
-
-# scale the data for the penalizer term
+# scale features
 x1_scaled = (x1 - mean(x1))/sd(x1)
 x2_scaled = (x2 - mean(x2))/sd(x2)
 x3_scaled = (x3 - mean(x3))/sd(x3)
 x4_scaled = (x4 - mean(x4))/sd(x4)
 
-# fit models and evaluate entropy
+# build candidate models
+full_model                    = lm(y ~ x1 + x2 + x3 + x4)
+full_model_scaled             = lm(y ~ x1_scaled + x2_scaled + x3_scaled + x4_scaled)
 
-# NOTE: the scaled models are only used for evaluating penalty term; not predictions.
+good_model                    = lm(y ~ x1 + x2)
+good_model_scaled             = lm(y ~ x1_scaled + x2_scaled)
 
-# NOTE 2: the "penalizer" takes the reciprocal of mean of the absolute value of the (scaled) coefficient estimates (excluding the intercept).
-# This is because the larger the coefficients, the closer this metric will be to zero and vice versa, if the coefficients are
-# close to zero then this metric grows large (and is infinity if any of the coefficients are exactly zero, which could be one
-# potential complication).
+bad_model                     = lm(y ~ x3 + x4)
+bad_model_scaled              = lm(y ~ x3_scaled + x4_scaled)
 
-# If you have a large model with a mot of redundant coefficients (near-zero), then this will drag the metric towards zero
-# and thus increase penalty. If you have a large model with a lot of informative coefficients (away from zero) then this will
-# drag the metric away from zero and thus decrease the penalty.
-full_mod = lm(y ~ x1 + x2 + x3 + x4)
-full_mod_scaled = lm(y ~ x1_scaled + x2_scaled + x3_scaled + x4_scaled)
-full_mod_penalizer = (1 / mean(abs(full_mod_scaled$coefficients[-1]))) # ignore intercept
-full_mod_entropy = full_mod_penalizer + -1*mean(log2(dnorm(y, mean = predict.lm(full_mod), sd = sd(residuals(full_mod)))))
+noisy_model_1                 = lm(y ~ x1 + x2 + x3)
+noisy_model_scaled_1          = lm(y ~ x1_scaled + x2_scaled + x3_scaled)
 
-x12_mod = lm(y ~ x1 + x2)
-x12_mod_scaled = lm(y ~ x1_scaled + x2_scaled)
-x12_mod_penalizer = (1 / mean(abs(x12_mod_scaled$coefficients[-1]))) # ignore intercept
-x12_mod_entropy = x12_mod_penalizer + -1*mean(log2(dnorm(y, mean = predict.lm(x12_mod), sd = sd(residuals(x12_mod)))))
+noisy_model_2                 = lm(y ~ x1 + x2 + x4)
+noisy_model_scaled_2          = lm(y ~ x1_scaled + x2_scaled + x4_scaled)
 
-x123_mod = lm(y ~ x1 + x2 + x3)
-x123_mod_scaled = lm(y ~ x1_scaled + x2_scaled + x3_scaled)
-x123_mod_penalizer = (1 / mean(abs(x123_mod_scaled$coefficients[-1]))) # ignore intercept
-x123_mod_entropy = x123_mod_penalizer + -1*mean(log2(dnorm(y, mean = predict.lm(x123_mod), sd = sd(residuals(x123_mod)))))
+partial_good_model_1          = lm(y ~ x1 + x3)
+partial_good_model_scaled_1   = lm(y ~ x1_scaled + x3_scaled)
 
-x13_mod = lm(y ~ x1 + x3)
-x13_mod_scaled = lm(y ~ x1_scaled + x3_scaled)
-x13_mod_penalizer = (1 / mean(abs(x13_mod_scaled$coefficients[-1]))) # ignore intercept
-x13_mod_entropy = x13_mod_penalizer + -1*mean(log2(dnorm(y, mean = predict.lm(x13_mod), sd = sd(residuals(x13_mod)))))
+partial_good_model_2          = lm(y ~ x2 + x3)
+partial_good_model_scaled_2   = lm(y ~ x2_scaled + x3_scaled)
 
-x34_mod = lm(y ~ x3 + x4)
-x34_mod_scaled = lm(y ~ x3_scaled + x4_scaled)
-x34_mod_penalizer = (1 / mean(abs(x34_mod_scaled$coefficients[-1]))) # ignore intercept
-x34_mod_entropy = x34_mod_penalizer + -1*mean(log2(dnorm(y, mean = predict.lm(x34_mod), sd = sd(residuals(x34_mod)))))
+# compute penalty terms
+full_model_penalty            = mean(exp(-abs(full_model_scaled$coefficients[-1])))
 
-full_mod_entropy
-x12_mod_entropy
-x123_mod_entropy
-x13_mod_entropy
-x34_mod_entropy
+good_model_penalty            = mean(exp(-abs(good_model_scaled$coefficients[-1])))
 
-# as a sanity check, we can compare against AIC
-AIC(full_mod)
-AIC(x12_mod)
-AIC(x123_mod)
-AIC(x13_mod)
-AIC(x34_mod)
+bad_model_penalty             = mean(exp(-abs(bad_model_scaled$coefficients[-1])))
 
-# generate "unseen" data
+noisy_model_1_penalty         = mean(exp(-abs(noisy_model_scaled_1$coefficients[-1])))
+
+noisy_model_2_penalty         = mean(exp(-abs(noisy_model_scaled_2$coefficients[-1])))
+
+partial_good_model_1_penalty  = mean(exp(-abs(partial_good_model_scaled_1$coefficients[-1])))
+
+partial_good_model_2_penalty  = mean(exp(-abs(partial_good_model_scaled_2$coefficients[-1])))
+
+# compute entropy
+full_model_entropy            = -mean(log2(dnorm(y, mean = predict.lm(full_model_scaled), sd = sd(residuals(full_model_scaled)))))
+
+good_model_entropy            = -mean(log2(dnorm(y, mean = predict.lm(good_model_scaled), sd = sd(residuals(good_model_scaled)))))
+
+bad_model_entropy             = -mean(log2(dnorm(y, mean = predict.lm(bad_model_scaled), sd = sd(residuals(bad_model_scaled)))))
+
+noisy_model_1_entropy         = -mean(log2(dnorm(y, mean = predict.lm(noisy_model_scaled_1), sd = sd(residuals(noisy_model_scaled_1)))))
+
+noisy_model_2_entropy         = -mean(log2(dnorm(y, mean = predict.lm(noisy_model_scaled_2), sd = sd(residuals(noisy_model_scaled_2)))))
+
+partial_good_model_1_entropy  = -mean(log2(dnorm(y, mean = predict.lm(partial_good_model_scaled_1), sd = sd(residuals(partial_good_model_scaled_1)))))
+
+partial_good_model_2_entropy  = -mean(log2(dnorm(y, mean = predict.lm(partial_good_model_scaled_2), sd = sd(residuals(partial_good_model_scaled_2)))))
+
+# model selection metric
+full_model_info           = full_model_penalty * full_model_entropy
+
+good_model_info           = good_model_penalty * good_model_entropy
+
+bad_model_info            = bad_model_penalty * bad_model_entropy
+
+noisy_model_1_info        = noisy_model_1_penalty * noisy_model_1_entropy
+
+noisy_model_2_info        = noisy_model_2_penalty * noisy_model_2_entropy
+
+partial_good_model_1_info = partial_good_model_1_penalty * partial_good_model_1_entropy
+
+partial_good_model_2_info = partial_good_model_2_penalty * partial_good_model_2_entropy
+
+# comparing custom metric with AIC
+metric_vs_AIC = as.data.frame(cbind(
+  c(full_model_info, good_model_info, bad_model_info, noisy_model_1_info, noisy_model_2_info, partial_good_model_1_info, partial_good_model_2_info),
+  c(AIC(full_model_scaled), AIC(good_model_scaled), AIC(bad_model_scaled), AIC(noisy_model_scaled_1), AIC(noisy_model_scaled_2), AIC(partial_good_model_scaled_1), AIC(partial_good_model_scaled_2))
+))
+names(metric_vs_AIC) = c("custom_metric", "AIC")
+metric_vs_AIC
+
+# generate unseen data
+# NOTE: overriding the old x1, ..., x4 so the predict() function works properly
 x1 = c()
 x2 = c()
 x3 = c()
@@ -81,18 +108,34 @@ y = c()
 for (i in 1:1000)
 {
   x1[i] = rnorm(1, mean = 2, sd = 1)
-  x2[i] = rnorm(1, mean = 5, sd = 3.5)
-  x3[i] = rnorm(1, mean = -1, sd = 2)
-  x4[i] = rnorm(1, mean = 3, sd = 2)
-  y[i] = 3*x1[i] + 1*x2[i] + 0*x3[i] + 0*x4[i] + 10 + rnorm(1, mean = 0, sd = 4)
+  x2[i] = rnorm(1, mean = -1, sd = 2)
+  x3[i] = rnorm(1, mean = 3.5, sd = 2.2)
+  x4[i] = rnorm(1, mean = 1, sd = 4)
+  y[i] = 3*x1[i] + 2*x2[i] + 0*x3[i] + 0*x4[i] + 10 + rnorm(1, mean = 0, sd = 4)
 }
+unseen_data = as.data.frame(cbind(x1, x2, x3, x4))
 
-# evaluate root mean squared error
-test_data = as.data.frame(cbind(x1, x2, x3, x4))
+# evaluate RMSE of models on unseen data
+full_model_rmse = sqrt(mean((y - predict.lm(full_model, newdata = unseen_data))^2))
 
-sqrt(mean((y - predict.lm(full_mod, newdata = test_data))^2))
-sqrt(mean((y - predict.lm(x12_mod, newdata = test_data))^2))
-sqrt(mean((y - predict.lm(x123_mod, newdata = test_data))^2))
-sqrt(mean((y - predict.lm(x13_mod, newdata = test_data))^2))
-sqrt(mean((y - predict.lm(x34_mod, newdata = test_data))^2))
-sqrt(mean((y - mu_y)^2))
+good_model_rmse = sqrt(mean((y - predict.lm(good_model, newdata = unseen_data))^2))
+
+bad_model_rmse = sqrt(mean((y - predict.lm(bad_model, newdata = unseen_data))^2))
+
+noisy_model_1_rmse = sqrt(mean((y - predict.lm(noisy_model_1, newdata = unseen_data))^2))
+
+noisy_model_2_rmse = sqrt(mean((y - predict.lm(noisy_model_2, newdata = unseen_data))^2))
+
+partial_good_model_1_rmse = sqrt(mean((y - predict.lm(partial_good_model_1, newdata = unseen_data))^2))
+
+partial_good_model_2_rmse = sqrt(mean((y - predict.lm(partial_good_model_2, newdata = unseen_data))^2))
+
+
+# compare custom metric to RMSE values on unseen data
+metric_vs_RMSE = as.data.frame(cbind(
+  c(full_model_info, good_model_info, bad_model_info, noisy_model_1_info, noisy_model_2_info, partial_good_model_1_info, partial_good_model_2_info),
+  c(AIC(full_model_scaled), AIC(good_model_scaled), AIC(bad_model_scaled), AIC(noisy_model_scaled_1), AIC(noisy_model_scaled_2), AIC(partial_good_model_scaled_1), AIC(partial_good_model_scaled_2)),
+  c(full_model_rmse, good_model_rmse, bad_model_rmse, noisy_model_1_rmse, noisy_model_2_rmse, partial_good_model_1_rmse, partial_good_model_2_rmse)
+))
+names(metric_vs_RMSE) = c("custom_metric", "AIC", "RMSE")
+metric_vs_RMSE
